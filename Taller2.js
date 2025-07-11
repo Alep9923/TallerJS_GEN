f39be615636fa340bb15ed58fa8dc1b64511c859
Alep9923 <alepalma9923@gmail.com>
2025-07-11 11:40:42 -0500

    Entrega de ejercicio 4 del taller

--- Taller2.js
+++ Taller2.js
@@ -68,3 +68,24 @@ if (producto) {
   console.log("Producto encontrado: " + producto.nombre + " - " + producto.precio);
 } else {
   console.log("Producto no encontrado");
+}
+
+//& Ejericio 4: Calcular Promedio de Notas
+//* Crear una función calcularPromedio(notas) que reciba un array de notas dado por el usuario separado por comas (,). La función debe retornar el promedio de las notas redondeado a 2 decimales.
+
+function calcularPromedio(notas) {
+  let suma = 0;
+  for (let i = 0; i < notas.length; i++) { //* Recorre el array de notas
+    suma += parseFloat(notas[i]); //* Convierte el string a float para sumarlo
+  }
+  let promedio = suma / notas.length; //* Calcula el promedio dado nota.lenght indica el numero de notas
+  return promedio.toFixed(2); //* Redondea el promedio a 2 decimales
+}
+
+let notas = prompt("Ingrese sus notas separadas por comas: ").split(",");
+let promedio = calcularPromedio(notas);
+
+console.log("Promedio de notas: " + promedio);
+
+//& Ejercicio 5: Filtrar usuarios por edad
+
